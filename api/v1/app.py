9217b2f631b7c7630900b650934e83fb8adb7a14
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-29 17:34:11 +0100

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ module to all routes of v1 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 
 
@@ -14,8 +14,22 @@ def close(self):
     """close storage function after close it"""
     storage.close()
 
-
+# Register the blueprint
 app.register_blueprint(app_views)
+
+# Define a custom error handler for 404 errors
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Custom error handler for 404 Not Found errors.
+
+    Args:
+        error: The error object (not used).
+
+    Returns:
+        A JSON response with a 404 status code.
+    """
+    return (jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
